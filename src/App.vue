026269e05fb6f68df161e661edfcc9ab5d50f144
Allan Cruvinel <allan.cruvinel@gmail.com>
2021-05-27 18:05:16 -0300

    primeiro commit, treino de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="" id="app">
+    <div class="container grid-xs py-2">
+      <img
+        class="img-responsive img-logo"
+        src="@/assets/logo.png"
+        alt="logomarca"
+      />
+      <form @submit.prevent="addTodo(todo)"> <!-- cria um form passando o objeto todo como parametro para a função, com o reload da pagina desativado -->
+        <div class="input-group">
+          <input
+            type="text"
+            v-model="todo.frase" 
+            class="form-input"
+            placeholder="Novo todo"
+          /> <!-- v-model espera o valor dentro do unput com a variavel da função reativa Data() -->
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+      </form>
+      <div class="todo-list">
+        <Todo v-for="t in todos" :key="t.id" @toogle="toogleTodo" :todo="t"/> <!--  -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {Todo}, //traz o componente template para realizar o for de itens
+  data() {
+    return { todos: [], todo: { checked : false } }; // seta as variáveis de evento que serão alteradas instantâneamente
+  },
+  methods: {
+    addTodo() {
+      this.todo.id = Date.now();
+      this.todos.push(this.todo);
+      this.todo = { checked : false};
+    },
+
+    toogleTodo(todo) {
+      const index = this.todos.findIndex(item => item.id === todo.id)
+      if (index > -1){
+        const checked = !this.todos[index].checked
+        this.$set(this.todos,index,{...this.todos[index],checked});
+      }
+    }
   }
-}
+}; //https://youtu.be/rcDm0UBfXpM?t=510 Continuar a Aula, https://youtu.be/cSa-SMVMGsE?t=965 (aula opcional)
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.img-logo {
+  max-width: 200px;
+  margin: 0 auto;
+}
+.todo-list {
+  padding-top: 1rem;
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tile">
+    <div class="tile-icon">
+      <div>
+        <i class="icon icon-time centered"></i>
+      </div>
+    </div>
+    <div class="tile-content">
+      <p class="tiles-subtitle">{{ todo.frase }}</p>
+      <div>{{todo.checked}}</div>
+    </div>
+    <div class="tile-action">
+      <button @click="$emit('toogle',todo)" class="btn btn-link">Concluir</button>
+      <button class="btn btn-link">
+        <span class="text-error">Remover</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { todo: { type: Object, required: true } },
+};
+</script>
